04bd325c76a7bcc296ed406163f56804d4a5da44
odedev <odedev@outlook.com>
2023-10-31 14:58:26 +0800

    update

--- app-tool/src/proxy.js
+++ app-tool/src/proxy.js
@@ -19,3 +19,37 @@ const options = {
 const proxy = createProxyMiddleware(options);
 
 export default proxy;
+
+
+
+// import { createProxyMiddleware } from 'http-proxy-middleware';
+// import app from './app.js'
+
+// const proxy = 'http://127.0.0.1:8080/'
+// let staticDir = 'public'
+
+
+
+// app.use(
+//   '/api',
+//   createProxyMiddleware({
+//     target: proxy,
+//     changeOrigin: true,
+//     pathRewrite: {
+//       '^/api' : ''
+//     }
+//   })
+// );
+
+// app.use(
+//   '/file/api',
+//   createProxyMiddleware({
+//     target: proxy,
+//     changeOrigin: true,
+//     pathRewrite: {
+//       '^/file/api' : '/file'
+//     }
+//   })
+// );
+
+// app.use('/file', createProxyMiddleware({target: proxy, changeOrigin: true}));
