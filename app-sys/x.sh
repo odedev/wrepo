48224dc4f2126f5e8e19853d724faa8dcbffd98d
odedev <odedev@outlook.com>
2023-11-03 17:11:21 +0800

    sys

---
+++ app-sys/x.sh
@@ -0,0 +1,31 @@
+
+
+go mod init app-sys
+
+go get -u github.com/gin-gonic/gin
+go get -u github.com/gin-contrib/gzip
+go get -u github.com/gin-contrib/cors
+go get -u github.com/gin-contrib/sse
+go get -u github.com/gorilla/websocket
+
+go get -u github.com/gorilla/csrf
+go get -u github.com/gorilla/sessions
+go get -u github.com/gorilla/securecookie
+
+go get -u github.com/golang-jwt/jwt/v5
+
+go get -u gorm.io/gorm
+go get -u gorm.io/driver/sqlite
+# go get -u gorm.io/driver/mysql
+go get -u github.com/mattn/go-sqlite3@latest
+
+go get -u go.etcd.io/bbolt@latest
+
+go get -u github.com/google/uuid
+
+go get -u go.uber.org/zap
+
+
+npm create vue@latest web
+
+
