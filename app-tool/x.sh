3859d836ac3854c6f9e79e60c995a12d72fb1ab3
odedev <odedev@outlook.com>
2023-11-03 17:10:58 +0800

    tool

---
+++ app-tool/x.sh
@@ -0,0 +1,10 @@
+
+npm install --save axios compression cors dotenv
+
+npm install --save express hbs helmet http-errors http-proxy-middleware
+npm install --save morgan multer mysql2
+npm install --save puppeteer reflect-metadata serve-favicon
+npm install --save sqlite3 typeorm uuid nanoid
+npm install --save wait-on ws
+
+npm install --save-dev @types/express @types/node nodemon tailwindcss
