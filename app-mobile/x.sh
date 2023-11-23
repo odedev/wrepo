4f8483c3bc30499527f8a28d4cb01b1fcfd901f9
odedev <odedev@outlook.com>
2023-11-23 17:37:47 +0800

    update

---
+++ app-mobile/x.sh
@@ -0,0 +1,53 @@
+
+npm install
+
+npm install --save vue
+npm install --save vue-router
+npm install --save pinia
+npm install --save axios
+
+npm install --save @capacitor/core
+npm install --save @capacitor/android
+npm install --save @capacitor/ios
+npm install --save @capacitor/action-sheet
+npm install --save @capacitor/app-launcher
+npm install --save @capacitor/app
+npm install --save @capacitor/background-runner
+npm install --save @capacitor/browser
+npm install --save @capacitor/camera
+npm install --save @capacitor/clipboard
+npm install --save @capacitor/device
+npm install --save @capacitor/dialog
+npm install --save @capacitor/filesystem
+npm install --save @capacitor/geolocation
+npm install --save @capacitor/haptics
+npm install --save @capacitor/keyboard
+npm install --save @capacitor/local-notifications
+npm install --save @capacitor/motion
+npm install --save @capacitor/network
+npm install --save @capacitor/preferences
+npm install --save @capacitor/push-notifications
+npm install --save @capacitor/screen-orientation
+npm install --save @capacitor/screen-reader
+npm install --save @capacitor/share
+npm install --save @capacitor/splash-screen
+npm install --save @capacitor/status-bar
+npm install --save @capacitor/text-zoom
+npm install --save @capacitor/toast
+
+
+npm install --save-dev @capacitor/cli
+npm install --save-dev vite
+npm install --save-dev prettier
+npm install --save-dev eslint-plugin-vue
+npm install --save-dev eslint
+npm install --save-dev @vue/eslint-config-prettier
+npm install --save-dev @vitejs/plugin-vue
+npm install --save-dev @rushstack/eslint-patch
+
+
+npx cap init
+npx cap add android
+npx cap add ios
+
+# npx cap sync
